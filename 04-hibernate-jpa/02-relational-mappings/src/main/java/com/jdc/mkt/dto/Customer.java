6de0ecc60ffef77fa6c21507844b5b11654e3b3b
htikelinn <htikezawlinn@gmail.com>
2025-08-30 10:38:58 +0630

    Add customer update to run

--- 04-hibernate-jpa/02-relational-mappings/src/main/java/com/jdc/mkt/dto/Customer.java
+++ 04-hibernate-jpa/02-relational-mappings/src/main/java/com/jdc/mkt/dto/Customer.java
@@ -2,34 +2,24 @@ package com.jdc.mkt.dto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "customer_tbl")
+@Table(name = "contact_tbl")
 public class Customer {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	@Column(nullable = false,length = 45)
-	private String name;
-	
-	@Enumerated(EnumType.STRING)
-	private MemberType memberType;
-	
-	@OneToOne(mappedBy = "customer")
-	private Contact contact;
-	
-	public enum MemberType{
-		Silver,Gold,Platinum,Diamond
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+    @Column(unique = true, length = 45, nullable = false)
+    private String email;
+    @Column(length = 45)
+    private String primaryPhone;
+    @Column(length = 45)
+    private String secondaryPhone;
+    
 }

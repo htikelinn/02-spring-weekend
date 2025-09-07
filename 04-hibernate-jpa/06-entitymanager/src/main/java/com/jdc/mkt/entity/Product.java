efe72883d33da1d3d6bbd298b87a97bbfd1c5ad0
htikelinn <htikezawlinn@gmail.com>
2025-09-07 10:10:18 +0630

    update persistance

--- 04-hibernate-jpa/06-entitymanager/src/main/java/com/jdc/mkt/entity/Product.java
+++ 04-hibernate-jpa/06-entitymanager/src/main/java/com/jdc/mkt/entity/Product.java
@@ -1,11 +1,8 @@
 package com.jdc.mkt.entity;
 
-import java.util.List;
-
 import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -23,18 +20,17 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	@Column(nullable = false,length = 45)
+
+	@Column(nullable = false, length = 45)
 	private String name;
-	
+
 	@ColumnDefault("1.0")
 	private double price;
-	
+
 	@ColumnDefault("true")
 	private boolean active;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Category category;
-	
-	
+
 }
